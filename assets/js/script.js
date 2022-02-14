ee4a476429b07feac55cd73f70ac39526c1949d4
Jaxon Adams <jadamsresume1@gmail.com>
2022-02-14 16:40:03 -0700

    Answers (finally) validated! Yaygit add .

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,53 +76,173 @@ var askQuestions = function() {
     displayQuestionOne();
 }
 
-var displayQuestionFive = function() {
+// Question four correct, display question five
+var displayQuestionFiveCorrect = function() {
     document.querySelector("#question-four").classList.remove("show");
     document.querySelector("#question-four").classList.add("hide");
 
     document.querySelector("#question-five").classList.remove("hide");
     document.querySelector("#question-five").classList.add("show");
 
-    $(".answer-btn").on("click", function() {
-        console.log("end of game");
+    alert("Correct!");
+
+    $(".answer-5a").on("click", function() {
+        alert("Incorrect!");
+        gameTimerNum = gameTimerNum - 10;
+    });
+    $(".answer-5b").on("click", function() {
+        alert("Incorrect!");
+        gameTimerNum = gameTimerNum - 10;
+    });
+    $(".answer-5c").on("click", function() {
+        alert("Correct!");
+    });
+    $(".answer-5d").on("click", function() {
+        alert("Incorrect!");
+        gameTimerNum = gameTimerNum - 10;
     });
 }
 
-var displayQuestionFour = function() {
+// Question four incorrect
+var displayQuestionFiveIncorrect = function() {
+    gameTimerNum = gameTimerNum - 10;
+
+    document.querySelector("#question-four").classList.remove("show");
+    document.querySelector("#question-four").classList.add("hide");
+
+    document.querySelector("#question-five").classList.remove("hide");
+    document.querySelector("#question-five").classList.add("show");
+
+    alert("Incorrect!");
+
+    $(".answer-5a").on("click", function() {
+        alert("Incorrect!");
+        gameTimerNum = gameTimerNum - 10;
+    });
+    $(".answer-5b").on("click", function() {
+        alert("Incorrect!");
+        gameTimerNum = gameTimerNum - 10;
+    });
+    $(".answer-5c").on("click", function() {
+        alert("Correct!");
+    });
+    $(".answer-5d").on("click", function() {
+        alert("Incorrect!");
+        gameTimerNum = gameTimerNum - 10;
+    });
+}
+
+// Question three correct, display question four
+var displayQuestionFourCorrect = function() {
     document.querySelector("#question-three").classList.remove("show");
     document.querySelector("#question-three").classList.add("hide");
 
     document.querySelector("#question-four").classList.remove("hide");
     document.querySelector("#question-four").classList.add("show");
 
-    $(".answer-btn").on("click", displayQuestionFive);
+    alert("Correct!");
+
+    $(".answer-4a").on("click", displayQuestionFiveIncorrect);
+    $(".answer-4b").on("click", displayQuestionFiveIncorrect);
+    $(".answer-4c").on("click", displayQuestionFiveCorrect);
+    $(".answer-4d").on("click", displayQuestionFiveIncorrect);
 }
 
-var displayQuestionThree = function() {
+// Question three incorrect
+var displayQuestionFourIncorrect = function() {
+    gameTimerNum = gameTimerNum - 10;
+
+    document.querySelector("#question-three").classList.remove("show");
+    document.querySelector("#question-three").classList.add("hide");
+
+    document.querySelector("#question-four").classList.remove("hide");
+    document.querySelector("#question-four").classList.add("show");
+
+    alert("Incorrect!");
+
+    $(".answer-4a").on("click", displayQuestionFiveIncorrect);
+    $(".answer-4b").on("click", displayQuestionFiveIncorrect);
+    $(".answer-4c").on("click", displayQuestionFiveCorrect);
+    $(".answer-4d").on("click", displayQuestionFiveIncorrect);
+}
+
+// Question two correct, display question three
+var displayQuestionThreeCorrect = function() {
     document.querySelector("#question-two").classList.remove("show");
     document.querySelector("#question-two").classList.add("hide");
 
     document.querySelector("#question-three").classList.remove("hide");
     document.querySelector("#question-three").classList.add("show");
 
-    $(".answer-btn").on("click", displayQuestionFour);
+    alert("Correct!");
+
+    $(".answer-3a").on("click", displayQuestionFourIncorrect);
+    $(".answer-3b").on("click", displayQuestionFourIncorrect);
+    $(".answer-3c").on("click", displayQuestionFourIncorrect);
+    $(".answer-3d").on("click", displayQuestionFourCorrect);
 }
 
-var displayQuestionTwo = function() {
+// Question two incorrect
+var displayQuestionThreeIncorrect = function() {
+    gameTimerNum = gameTimerNum - 10;
+
+    document.querySelector("#question-two").classList.remove("show");
+    document.querySelector("#question-two").classList.add("hide");
+
+    document.querySelector("#question-three").classList.remove("hide");
+    document.querySelector("#question-three").classList.add("show");
+
+    alert("Incorrect!");
+
+    $(".answer-3a").on("click", displayQuestionFourIncorrect);
+    $(".answer-3b").on("click", displayQuestionFourIncorrect);
+    $(".answer-3c").on("click", displayQuestionFourIncorrect);
+    $(".answer-3d").on("click", displayQuestionFourCorrect);
+}
+
+// Question One Correct, display question two
+var displayQuestionTwoCorrect = function() {
     document.querySelector("#question-one").classList.remove("show");
     document.querySelector("#question-one").classList.add("hide");
 
     document.querySelector("#question-two").classList.remove("hide");
     document.querySelector("#question-two").classList.add("show");
 
-    $(".answer-btn").on("click", displayQuestionThree);
+    alert("Correct!");
+
+    $(".answer-2a").on("click", displayQuestionThreeIncorrect);
+    $(".answer-2b").on("click", displayQuestionThreeIncorrect);
+    $(".answer-2c").on("click", displayQuestionThreeCorrect);
+    $(".answer-2d").on("click", displayQuestionThreeIncorrect);
 }
 
+// Question One Incorrect
+var displayQuestionTwoIncorrect = function() {
+    gameTimerNum = gameTimerNum - 10;
+
+    document.querySelector("#question-one").classList.remove("show");
+    document.querySelector("#question-one").classList.add("hide");
+
+    document.querySelector("#question-two").classList.remove("hide");
+    document.querySelector("#question-two").classList.add("show");
+
+    alert("Incorrect!");
+
+    $(".answer-2a").on("click", displayQuestionThreeIncorrect);
+    $(".answer-2b").on("click", displayQuestionThreeIncorrect);
+    $(".answer-2c").on("click", displayQuestionThreeCorrect);
+    $(".answer-2d").on("click", displayQuestionThreeIncorrect);
+}
+
+// Display question one
 var displayQuestionOne = function() {
     document.querySelector("#question-one").classList.remove("hide");
     document.querySelector("#question-one").classList.add("show");
 
-    $(".answer-btn").on("click", displayQuestionTwo);
+    $(".answer-1a").on("click", displayQuestionTwoIncorrect);
+    $(".answer-1b").on("click", displayQuestionTwoIncorrect);
+    $(".answer-1c").on("click", displayQuestionTwoCorrect);
+    $(".answer-1d").on("click", displayQuestionTwoIncorrect);
 }
 
 startButtonEl.addEventListener("click", startGame);
